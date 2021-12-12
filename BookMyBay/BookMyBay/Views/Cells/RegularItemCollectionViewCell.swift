0585e78e0e9b50018fc7cc47538d78c07ab6309a
Juan Antonio Naranjo García <antonio.naranjo@pragma.com.co>
2021-12-12 17:03:49 -0500

    - Base project and initial architecture added.

---
+++ BookMyBay/BookMyBay/Views/Cells/RegularItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RegularItemCollectionViewCell.swift
+//  BookMyBay
+//
+//  Created by TonyJANG on 12/12/21.
+//
+
+import UIKit
+
+class RegularItemCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ BookMyBay/BookMyBay/Views/Cells/SeeMoreCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SeeMoreCollectionViewCell.swift
+//  BookMyBay
+//
+//  Created by TonyJANG on 12/12/21.
+//
+
+import UIKit
+
+class SeeMoreCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
